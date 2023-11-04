02b2846c156afc273792f726e2bb8971a7870882
HassanMak29 <76273304+HassanMak29@users.noreply.github.com>
2023-11-04 22:04:48 +0100

    fixed spinner color in dark mode

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,9 +1,16 @@
 import { Ring } from "@uiball/loaders";
+import { useTheme } from "../context/themeContext/useTheme";
 
 export default function Spinner() {
+  const { darkTheme } = useTheme();
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <Ring size={40} lineWeight={5} speed={2} color="black" />
+      <Ring
+        size={40}
+        lineWeight={5}
+        speed={2}
+        color={darkTheme ? "white" : "black"}
+      />
     </div>
   );
 }
